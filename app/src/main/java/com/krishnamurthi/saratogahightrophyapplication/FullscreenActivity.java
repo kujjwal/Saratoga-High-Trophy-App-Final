6248f83dc90ded25cfc584a2c68e0ec17bdaf1c0
Arman R <armanr123@gmail.com>
2020-01-11 15:38:14 -0800

    added toolbar + search bar

--- app/src/main/java/com/krishnamurthi/saratogahightrophyapplication/FullscreenActivity.java
+++ app/src/main/java/com/krishnamurthi/saratogahightrophyapplication/FullscreenActivity.java
@@ -109,8 +109,8 @@ public class FullscreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fullscreen);
 
         mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
+       // mControlsView = findViewById(R.id.fullscreen_content_controls);
+      //  mContentView = findViewById(R.id.fullscreen_content);
 
         Context context = getApplicationContext();
         DevicePolicyManager dpm = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
@@ -128,7 +128,7 @@ public class FullscreenActivity extends AppCompatActivity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        //findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
